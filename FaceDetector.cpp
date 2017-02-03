8047af93dfc49445b471c503b54ba16e0549ec80
Giovanni Murru <CynnyDev@linfa.local>
2017-02-03 17:32:50 +0100

    creation of file FaceDetector

---
+++ FaceDetector.cpp
@@ -0,0 +1,9 @@
+//
+//  GMFaceDetector.cpp
+//  opencv-osx
+//
+//  Created by Giovanni Murru on 03/02/2017.
+//  Copyright © 2017 Giovanni Murru. All rights reserved.
+//
+
+#include "GMFaceDetector.hpp"
---
+++ FaceDetector.hpp
@@ -0,0 +1,18 @@
+//
+//  GMFaceDetector.hpp
+//  opencv-osx
+//
+//  Created by Giovanni Murru on 03/02/2017.
+//  Copyright © 2017 Giovanni Murru. All rights reserved.
+//
+
+#ifndef GMFaceDetector_hpp
+#define GMFaceDetector_hpp
+
+#include <stdio.h>
+class GMFaceDetector
+{
+    
+};
+
+#endif /* GMFaceDetector_hpp */
